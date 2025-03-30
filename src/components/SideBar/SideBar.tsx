76c2fbc3512d9f1b9f306d3b7f242b0ac1fd912b
Milton Kiefer <145204997+Miltchola@users.noreply.github.com>
2025-03-30 13:32:39 -0300

    Add files via upload

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,5 +1,13 @@
 /* import React from "react"; */
 import "./SideBar.css";
+import star from '../../assets/icons/star.png';
+import fire from '../../assets/icons/fire.png';
+import podium from '../../assets/icons/podium.png';
+import trophey from '../../assets/icons/trophey.png';
+import speed from '../../assets/icons/speed.png';
+
+
+
 
 const SideBar = () => {
     return <div className="sidebar">
@@ -12,24 +20,20 @@ const SideBar = () => {
 
         <div className="sections">
             <div className="sections_button">
-                <img src="" alt="" />
+                <img className="section-icon" src={star} alt="" />
                 <h6 className="section-text">Last 30 days</h6>
             </div>
 
             <div className="sections_button">
-                <img src="" alt="" />
+                <img className="section-icon" src={fire} alt="" />
                 <h6 className="section-text">This week</h6>
             </div>
 
             <div className="sections_button">
-                <img src="" alt="" />
+                <img className="section-icon" src={speed} alt="" />
                 <h6 className="section-text">Next week</h6>
             </div>
 
-            <div className="sections_button">
-                <img src="" alt="" />
-                <h6 className="section-text">Release calendar</h6>
-            </div>
         </div>
     </div>
 }
