6fa63fb5e0f2a046f4c833bc53f22b3d5a79fb68
Luccas Pereira de Pino <luccaspino@icloud.com>
2025-04-10 15:13:35 -0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,7 @@ const Header = ({ toggleSidebar, setSearchQuery }: { toggleSidebar: () => void; 
           <button className="menu-button" onClick={toggleSidebar}>
             <img className="logo" src={logo} alt="Site Logo" />
           </button>
-          <img className="logo" src="/path-to-logo.png" alt="Site Logo" />
+          <img className="logo" src="/assets/games-DX9OXY3m.png" alt="Site Logo" />
           <h1 className="site-title">Hot and Trending</h1>
         </div>
   
